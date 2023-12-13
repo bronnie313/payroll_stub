90bd4dd8ee7281606a7173d9a504562c158ff436
Bisaso Ronald <bronnie313@gmail.com>
2023-12-13 16:13:53 +0300

    refactor code to remove repetitive loops

--- invoice_program.rb
+++ invoice_program.rb
@@ -1,11 +1,8 @@
 # add the price of items all together for each item on the list
 def total (prices)
     amount = 0
-    index = 0
-
-    while index < prices.length
-        amount += prices[index]
-        index += 1
+    prices.each do |price|
+        amount += price
     end
     amount
 end
@@ -17,13 +14,12 @@ puts format("%.2f", total(prices))
 
 def refund(prices)
     amount = 0
-    index = 0
-    while index < prices.length
-        amount -= prices[index]
-        index += 1
+    prices.each do |price| 
+        amount -= price
     end
     amount 
 end
+
 puts "---"
 prices = [3.99, 25.00, 8.99]
 puts format("%.2f", refund(prices))
@@ -32,10 +28,9 @@ puts "---"
 #  reduce each item's price by 1/3 & print the savings
 def show_discounts(prices)
     index = 0
-    while index < prices.length
-        amount_off = prices[index] / 3.0
-        puts format("%.2f", amount_off)
-        index += 1
+    prices.each do |price|
+        amount_off = price / 3.0
+        puts format("Your Discount: %.2f", amount_off)
     end
 end
 
