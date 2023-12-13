f7439a1d6df53b5aa61680d2a9be41684bc530ae
Bisaso Ronald <bronnie313@gmail.com>
2023-12-13 10:37:31 +0300

    add method to show discounts for each price

--- invoice_program.rb
+++ invoice_program.rb
@@ -27,3 +27,16 @@ end
 puts "---"
 prices = [3.99, 25.00, 8.99]
 puts format("%.2f", refund(prices))
+puts "---"
+
+#  reduce each item's price by 1/3 & print the savings
+def show_discounts(prices)
+    index = 0
+    while index < prices.length
+        amount_off = prices[index] / 3.0
+        puts format("%.2f", amount_off)
+        index += 1
+    end
+end
+
+show_discounts(prices)
