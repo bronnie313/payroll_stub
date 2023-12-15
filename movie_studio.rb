ba635e149e368b681c0809e56634cc0a14568b75
Bisaso Ronald <bronnie313@gmail.com>
2023-12-15 12:33:41 +0300

    use find_all method to search lines that include trucated

--- movie_studio.rb
+++ movie_studio.rb
@@ -1,10 +1,19 @@
-review_file = File.open("reviews.txt")
-# puts review_file.class
+lines = []
 
-lines = review_file.readlines
-puts "Line 4: #{lines[3]}"
-puts "Line 1: #{lines[0]}"
+File.open("reviews.txt") do |review_file| 
+    lines = review_file.readlines
+end
 
-review_file.close
+# relevant_lines = []
+
+# lines.each do |line|
+#     if line.include?("Truncated")
+#         relevant_lines << line
+#     end
+# end
+
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+
+puts relevant_lines
 
 
